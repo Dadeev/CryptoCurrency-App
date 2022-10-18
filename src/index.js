1233cc50c45f7107ef84d4b32ea3e7de8e7fadfe
Tima <dadeev.tim@gmail.com>
2022-10-18 20:52:36 +0700

    feat: implement Api and use millify function to convert long number to pretty

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
 import {App} from "./App";
 import 'antd/dist/antd.css'
+import {Provider} from "react-redux";
+import store from "./app/store";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root')
 );
 root.render(
     <BrowserRouter>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </BrowserRouter>
 );
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,23 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '2cdb13ac11msh55aa4718edd6c7dp130468jsnc81b21798e6c',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi
