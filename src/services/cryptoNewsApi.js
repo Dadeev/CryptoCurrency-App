0e3c1c83ced38d131746c839a96229da335a36d9
Tima <dadeev.tim@gmail.com>
2022-10-19 20:20:24 +0700

    feat: implement news page

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -11,12 +11,12 @@ const createRequest = (url) => ({url, headers: cryptoNewsHeaders})
 
 export const cryptoNewsApi = createApi({
     reducerPath: 'cryptoNewsApi',
-    baseQuery: fetchBaseQuery({baseUrl}),
+    baseQuery: fetchBaseQuery({ baseUrl}),
     endpoints: (builder) => ({
         getCryptoNews: builder.query({
-            query: (newsCategory, count) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
-        })
-    })
-})
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+        }),
+    }),
+});
 
-export const {useGetCryptosNewsQuery} = cryptoNewsApi
+export const {useGetCryptoNewsQuery} = cryptoNewsApi
